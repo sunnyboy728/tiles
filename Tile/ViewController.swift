6b3a94a5671b878d522d5ef6e1ec6cec037d1b34
Brandon Chan <brandon@Meiyis-MacBook-Pro.local>
2014-12-14 20:31:48 -0500

    commit Sunday night

--- Tile/ViewController.swift
+++ Tile/ViewController.swift
@@ -8,10 +8,8 @@
 
 import UIKit
 
-var tileImg:[UIImage] = []
-
-let rows:Int = 10
-let columns:Int = 10
+let rows:Int = 3
+let columns:Int = 5
 
 let screenRows:Int = 5
 let screenColumns:Int = 5
@@ -21,34 +19,63 @@ let left:Int = ((screenRows - 1) / 2) * screenColumns + ((screenColumns - 1) / 2
 let right:Int = ((screenRows - 1) / 2) * screenColumns + ((screenColumns - 1) / 2) + 1
 let bottom:Int = ((screenRows - 1) / 2 + 1) * screenColumns + ((screenColumns - 1) / 2)
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    var tileImgs:anyValueStorage<UIImage> = anyValueStorage(@IBOutlet var tileImg1: UIImageView!, @IBOutlet var tileImg2: UIImageView!, @IBOutlet var tileImg3: UIImageView!, @IBOutlet var tileImg4: UIImageView!, @IBOutlet var tileImg5: UIImageView!, @IBOutlet var tileImg6: UIImageView!, @IBOutlet var tileImg7: UIImageView!, @IBOutlet var tileImg8: UIImageView!, @IBOutlet var tileImg9: UIImageView!, @IBOutlet var tileImg10: UIImageView!, @IBOutlet var tileImg11: UIImageView!, @IBOutlet var tileImg12: UIImageView!, @IBOutlet var tileImg13: UIImageView!, @IBOutlet var tileImg14: UIImageView!, @IBOutlet var tileImg15: UIImageView!)
+    
     var myMap = Map(numRows:rows, numColumns:columns, numScreenRows:screenRows, numScreenColumns:screenColumns)
+    
     var myCurrentX = 2
     var myCurrentY = 2
+    
+    
     
     @IBAction func clickTile(sender:UIButton)
     {
         var currentPosition:Int = myCurrentX + myCurrentY * screenColumns
         PositionControl.correctOvergo(map:myMap, row:&myCurrentX, column:&myCurrentY)
         PositionControl.updatePosition(position:currentPosition, row:&myCurrentY, column:&myCurrentX)
-        myMap.drawScreen(rowNum:myCurrentX, columnNum:myCurrentY)
+        myMap.drawScreen(rowNum:myCurrentX, columnNum:myCurrentY, images:tileImgs)
     }
 }
 
 func getRandomNumber(#range:Range<UInt32>) -> Int
 {
     return Int(range.startIndex + arc4random_uniform(range.endIndex - range.startIndex + 1))
+}
+
+class anyValueStorage<T>
+{
+    var valueArray:[T] = []
+    init(values:T...)
+    {
+        for value:T in values
+        {
+            valueArray.append(value)
+        }
+    }
+    
+    func setForIndex(#index:Int, value:T)
+    {
+        valueArray[index] = value
+    }
+    
+    subscript(#index:Int) -> T
+    {
+        return valueArray[index]
+    }
 }
 
 class Map
@@ -58,6 +85,7 @@ class Map
     var columns:Int
     var screenRows:Int
     var screenColumns:Int
+    
     init(numRows:Int, numColumns:Int, numScreenRows:Int, numScreenColumns:Int)
     {
         rows = numRows
@@ -74,7 +102,8 @@ class Map
             map.append(newArray)
         }
     }
-    func setImage(#tileImageNum:Int, inputTileImageType:Int)
+    
+    func setImage(#tileImageNum:Int, inputTileImageType:Int, images:anyValueStorage<UIImage>)
     {
         var tileImageType:String = ""
         switch inputTileImageType
@@ -92,7 +121,7 @@ class Map
                 break;
         }
         
-        tileImg[tileImageNum] = UIImage(named:tileImageType)!
+        images.setForIndex(index: tileImageNum, value:UIImage(named:tileImageType)!)
     }
     
     func getRows() -> Int
@@ -105,16 +134,15 @@ class Map
         return columns
     }
         
-    func drawScreen(#rowNum:Int, columnNum:Int)
+    func drawScreen(#rowNum:Int, columnNum:Int, images:anyValueStorage<UIImage>)
     {
         var rowAndColumnController:Int = 0
         for imageNum in 0...screenRows * screenColumns
         {
-            setImage(tileImageNum:imageNum, inputTileImageType:self.map[(rowAndColumnController / 3) - (self.screenColumns - 1) / 2][(rowAndColumnController % 3) - (self.screenRows - 1) / 2])
+            self.setImage(tileImageNum:imageNum, inputTileImageType:self.map[(rowAndColumnController / 3) - (self.screenColumns - 1) / 2][(rowAndColumnController % 3) - (self.screenRows - 1) / 2], images:images)
             rowAndColumnController++
         }
     }
-    
     subscript(#row:Int, #column:Int) -> Int
     {
         return map[row][column]
